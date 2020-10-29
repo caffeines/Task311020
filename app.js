d37df81f0fb1055269da5e49029f37a5fcaad3c5
caffeines <sadat.talks@gmail.com>
2020-10-29 16:06:10 +0600

    app setup done

---
+++ app.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('./lib/mongoose');
+const config = require('./config');
+
+const app = express();
+app.use(helmet());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// app.use(response);
+
+mongoose.connectMongoDB();
+
+const port = Number(process.env.PORT) || config.server.port;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
--- config/mongo.js
+++ config/mongo.js
@@ -1,17 +1,7 @@
 const { formatEnv } = require('../lib/utils');
 
-const vars = formatEnv([
-  { name: 'MONGO_HOST', defaultValue: 'localhost' },
-  { name: 'MONGO_PORT', type: 'number', defaultValue: 27017 },
-  { name: 'MONGO_DATABASE_NAME' },
-  { name: 'MONGO_ROOT_USER' },
-  { name: 'MONGO_PASSWORD' },
-]);
+const vars = formatEnv([{ name: 'MONGODB_URL' }]);
 
 module.exports = {
-  host: vars.MONGO_HOST,
-  port: vars.MONGO_PORT,
-  database: vars.MONGO_DATABASE_NAME,
-  user: vars.MONGO_ROOT_USER,
-  password: vars.MONGO_PASSWORD,
+  mongodbURL: vars.MONGODB_URL,
 };
